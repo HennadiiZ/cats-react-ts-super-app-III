b9c96f6a7340decd58066030c150f848e8a63d1a
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-04-16 20:51:22 -0600

    update

--- src/UI/button/Button.tsx
+++ src/UI/button/Button.tsx
@@ -1,6 +1,6 @@
-import classes from './Button.module.scss';
 import React, { FC } from 'react';
 import { ButtonProps } from '../../interfaces/interfaces';
+import classes from './Button.module.scss';
 
 const Button: FC<ButtonProps> = ({ label, onClick, type = 'button' }) => {
     return (
